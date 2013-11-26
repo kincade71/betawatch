808f5592dd66224c329f602da3e4767cbc4cbb61
Richard Robinson <richard.robinson@forrent.com>
2013-11-26 12:20:15 -0500

    Deleted log files from dev.

--- application/logs/log-2013-11-26.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-11-26 18:18:12 --> Severity: Notice  --> Undefined variable: projectname /Users/richard.robinson/Documents/sites/betawatch/sparks/fire_log/0.8.2/views/fire_log_view.php 60
