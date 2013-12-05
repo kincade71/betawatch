bbb5a26a71cb76063d819800fdbe39fc79fd9be0
Richard Robinson <richard.robinson@forrent.com>
2013-12-05 17:42:11 -0500

    added history back in

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,8 +4,9 @@
 <div class="tabbable tabs-left"> 
   <ul class="nav nav-tabs">
     <li class="active"><a href="#tab1" data-toggle="tab">Overview</a></li>
+    <li><a href="#tab2" data-toggle="tab">History</a></li>
     <?php
-	$i = 2;
+	$i = 3;
     foreach($server as $tab){?>
 		<li><a href="#tab<?= $i ?>" data-toggle="tab"><?= $tab?></a></li>
 	 <?php $i++; } ?>
@@ -54,8 +55,52 @@
       
       ?></table>
     </div>
+    
+     <div class="tab-pane" id="tab2">
+    <h1 class="lead pull-right"><?= $whereiam?> history</h1>
+    <table class="table table-bordered table-striped table-hover">
+      <?php      	
+      	foreach ($history as $sd) {
+				echo '<tr class="';
+				switch ($sd->success) {
+				    case '1':
+				        echo "alert alert-success";
+				        break;
+				    case '0':
+				        echo "alert alert-error";
+				        break;
+				    case ($sd->datestamp - time())<=10:
+			    		echo "alert alert-warning";
+			    		break;
+				    default:
+				       echo "<strong>Code base not defined</strong><br/>";
+				}
+				echo'">';
+				echo "<td>";
+				echo $sd->server.' - ';
+				switch ($sd->codebase) {
+				    case 'fe':
+				        echo "Front End - ";
+				        break;
+				    case 'be':
+				        echo "Back End - ";
+				        break;
+				    default:
+				       echo ucfirst($sd->codebase).' - ';
+				}
+				echo "".$sd->release." - ";
+				echo "".$sd->datestamp."";
+				//echo "".$sd->md5."<br/>";
+				//echo "".$sd->server."<br/>";
+				echo "</tr>";
+      	}
+      	
+      
+      ?></table>
+    </div>
+    
  <?php 
- 	$i = 2;
+ 	$i = 3;
  	foreach($server as $tab){?>
  	<div class="tab-pane" id="tab<?= $i?>">
  	<h1 class="lead pull-right"><?= $tab?></h1>
